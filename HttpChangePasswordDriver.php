c82f3be8d27779731386014c40ab791a05e6ba3a
solarisfan <84895244+solarisfan@users.noreply.github.com>
2021-05-29 20:48:08 +0800

    Add files via upload
    Implement parameter substitution on URL

--- HttpChangePasswordDriver.php
+++ HttpChangePasswordDriver.php
@@ -26,9 +26,7 @@ class HttpChangePasswordDriver implements \RainLoop\Providers\ChangePassword\Cha
 	 * @var string
 	 */
 	private $sChangePassword2URL = '';
-	
-	private $sTestMode = '';
-	
+		
 	/**
 	 * @var
 	/**
@@ -73,6 +71,75 @@ class HttpChangePasswordDriver implements \RainLoop\Providers\ChangePassword\Cha
 	}
 
 	/**
+	 * @param $sUri
+	 * @param $sUser
+	 * @param $sDomain
+	 * @param $sOldpassword
+	 * @param $sNewpassword
+	 * @param $sTrailer
+	 * 
+	 * @return URI with substituted username, domain, old password and new password
+	 */
+	private function RewriteURI($sUri, $sUser, $sDomain, $sOldpassword=false, 
+								$sNewpassword=false) {
+		$qry=false;
+		$tok = strtok($sUri, '&');
+		while ($tok !== false) {
+			if ($qry) $qry .= '&';
+			$tmp = str_replace('%USER%', urlencode($sUser), $tok, $cnt);
+			if ($cnt < 1) $tmp = str_replace('%DOMAIN%', urlencode($sDomain), $tok, $cnt);
+			if (($sOldpassword) && ($cnt < 1)) $tmp = str_replace('%OLDPASSWORD%', urlencode($sOldpassword), $tok, $cnt);
+			if (($sNewpassword) && ($cnt < 1)) $tmp = str_replace('%NEWPASSWORD%', urlencode($sNewpassword), $tok, $cnt);
+			$qry .= $tmp;
+			$tok = strtok('&');
+		}
+		return $qry;
+	}
+	
+	/**
+	 * @param $sURL
+	 * @param $sEmailUser
+	 * @param $sEmailDomain
+	 * @param $sOldpassword
+	 * @param $sNewpassword
+	 * @param $sTrailer
+	 * 
+	 * @return XML content from the URL and supplied parameters
+	 */
+	private function IssueURL($sUrl, $sEmailUser, $sEmailDomain, 
+								$sOldpassword=false, $sNewpassword=false,
+								$sTrailer='') {
+		$i= strpos($sUrl, '?');
+		$sHttp = substr($sUrl, 0, $i);
+		$sUri='';
+		if ($i) $sUri = substr($sUrl, $i+1);
+		$qry = $this->RewriteURI($sUri, $sEmailUser, 
+								$sEmailDomain, 
+								$sOldpassword, $sNewpassword);
+		if ($sTrailer) $qry .= $sTrailer;
+		$ch = curl_init($sHttp);
+		curl_setopt( $ch, CURLOPT_POST, 1);
+		curl_setopt( $ch, CURLOPT_POSTFIELDS, $qry);
+		curl_setopt( $ch, CURLOPT_FOLLOWLOCATION, 1);
+		curl_setopt( $ch, CURLOPT_HEADER, 0);
+		curl_setopt( $ch, CURLOPT_RETURNTRANSFER, 1);
+		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+		$xml = curl_exec( $ch );
+		$errorCode= curl_getinfo($ch, CURLINFO_HTTP_CODE);
+		if ($errorCode == 404) {
+			$xml = false;
+		}
+		curl_close($ch);
+		if ((!$xml) && ($this->oLogger)) 
+		{
+			$this->oLogger->Write('URL did not return any content: ' . $sUrl, 
+									\MailSo\Log\Enumerations\Type::ERROR);
+		}
+	
+		return $xml;
+	}
+	
+	/**
 	 * @param \RainLoop\Account $oAccount
 	 *
 	 * @return bool
@@ -87,23 +154,14 @@ class HttpChangePasswordDriver implements \RainLoop\Providers\ChangePassword\Cha
 		$sEmailUser = \MailSo\Base\Utils::GetAccountNameFromEmail($sEmail);
 		$sEmailDomain = \MailSo\Base\Utils::GetDomainFromEmail($sEmail);
 		if ($this->sPermissionURL) {
-			$ch = curl_init($this->sPermissionURL);
-			curl_setopt( $ch, CURLOPT_POST, 1);
-			$qry = 'user=' . urlencode($sEmailUser);
-			$qry .= '&domain=' . urlencode($sEmailDomain);
-			$qry .= $this->sTestMode;
-			curl_setopt( $ch, CURLOPT_POSTFIELDS, $qry);
-			curl_setopt( $ch, CURLOPT_FOLLOWLOCATION, 1);
-			curl_setopt( $ch, CURLOPT_HEADER, 0);
-			curl_setopt( $ch, CURLOPT_RETURNTRANSFER, 1);
-			curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-			$xml = curl_exec( $ch );
-			curl_close($ch);
+			$xml = $this->IssueURL($this->sPermissionURL, $sEmailUser, $sEmailDomain);
 			if ($xml) {
 				$token = simplexml_load_string($xml);
 				if ($token->enabled) {
 					if ($token->enabled == 'false') return false;
 				}
+			} else {
+				return false;
 			}
 		}
 		return $oAccount && $oAccount->Email() &&
@@ -126,40 +184,24 @@ class HttpChangePasswordDriver implements \RainLoop\Providers\ChangePassword\Cha
 		$sEmail = $oAccount->Email();
 		$sEmailUser = \MailSo\Base\Utils::GetAccountNameFromEmail($sEmail);
 		$sEmailDomain = \MailSo\Base\Utils::GetDomainFromEmail($sEmail);
-		$ch = curl_init($this->sChangePassword1URL);
-		curl_setopt( $ch, CURLOPT_POST, 1);
-		$qry = 'user=' . urlencode($sEmailUser);
-		$qry .= '&domain=' . urlencode($sEmailDomain);
-		$qry .= '&pwd=' . urlencode($sPrevPassword);
-		$qry .= $this->sTestMode;
-		curl_setopt( $ch, CURLOPT_POSTFIELDS, $qry);
-		curl_setopt( $ch, CURLOPT_FOLLOWLOCATION, 1);
-		curl_setopt( $ch, CURLOPT_HEADER, 0);
-		curl_setopt( $ch, CURLOPT_RETURNTRANSFER, 1);
-		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-		$xml = curl_exec( $ch );
-		curl_close($ch);
-		if (!$xml) return false;
-		$token = simplexml_load_string($xml);
-		$ary = '';
-		foreach($token as $k => $v) {
-			$ary .= '&' . $k . '=' . urlencode($v);
+		$sTrailer = '';
+		if ($this->sChangePassword1URL) {
+			$xml = $this->IssueURL($this->sChangePassword1URL, $sEmailUser, $sEmailDomain, $sPrevPassword);
+			if (!$xml) return false;
+			$token = simplexml_load_string($xml);
+			$ary = '';
+			foreach($token as $k => $v) {
+				$sTrailer .= '&' . $k . '=' . urlencode($v);
+			}
 		}
-		$ch = curl_init($this->sChangePassword2URL);
-		curl_setopt( $ch, CURLOPT_POST, 1);
-		$qry = 'pwd=' . urlencode($sNewPassword);
-		$qry .= $ary;
-		$qry .= $this->sTestMode;
-		curl_setopt( $ch, CURLOPT_POSTFIELDS, $qry);
-		curl_setopt( $ch, CURLOPT_FOLLOWLOCATION, 1);
-		curl_setopt( $ch, CURLOPT_HEADER, 0);
-		curl_setopt( $ch, CURLOPT_RETURNTRANSFER, 1);
-		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-		$xml = curl_exec( $ch );
-		curl_close($ch);
-		if (!$xml) return false;
-		$token = simplexml_load_string($xml);
-		if ($token->result == 'true') return true;
+		if ($this->sChangePassword2URL) {
+			$xml = $this->IssueURL($this->sChangePassword2URL, 
+						$sEmailUser, $sEmailDomain, false, 
+						$sNewPassword, $sTrailer);
+			if (!$xml) return false;
+			$token = simplexml_load_string($xml);
+			if ($token->result == 'true') return true;
+		}
 		return false;
 	}
 }
